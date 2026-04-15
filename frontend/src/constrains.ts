97c5aa5e20ea21d65246e3da7a9ab0a5109e00ed
Denys Bushuliak <denys.bushuliak@icloud.com>
2026-04-15 21:50:02 +0200

    fix: remove dependencies from API_URL

--- frontend/src/constrains.ts
+++ frontend/src/constrains.ts
@@ -1,8 +1,6 @@
-const API_URL = import.meta.env.VITE_API_URL;
-
 export const PULL_INTERVAL = 10000;
 export const MIN_VALUE = 0;
 export const MAX_VALUE = 1_000_000_000;
-export const INCREMENT_URL = `${API_URL}/increment`;
-export const DECREMENT_URL = `${API_URL}/decrement`;
-export const CURRENT_VALUE_URL = `${API_URL}/current`;
+export const INCREMENT_URL = `/increment`;
+export const DECREMENT_URL = `/decrement`;
+export const CURRENT_VALUE_URL = `/current`;
